cb088558cd5e115a13230d2472de51734148d184
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-05 08:57:15 +0900

    Add new package(python-get-absolute-resource-path)

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description='PyQt QPushButton which user can set svg icon (not a low quality)',
     url='https://github.com/yjg30737/pyqt-svg-icon-pushbutton.git',
     install_requires=[
-        'PyQt5>=5.8'
+        'PyQt5>=5.8',
+        'python-get-absolute-resource-path @ git+https://git@github.com/yjg30737/python-get-absolute-resource-path.git@main'
     ]
 )
