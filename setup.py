1ab17c4b956a50e6b28574e79a9c16cfb0b04eef
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-15 08:05:14 +0900

    Update version (0.6.0 -> 0.7.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-svg-icon-pushbutton',
-    version='0.6.0',
+    version='0.7.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
