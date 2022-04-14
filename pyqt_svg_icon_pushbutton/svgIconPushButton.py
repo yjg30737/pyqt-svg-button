caa7f6f1e2550972fb6fa334bad6ed6478f1c6fa
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-14 18:28:09 +0900

    Make button's auto brightness control feature more reliable

--- pyqt_svg_icon_pushbutton/svgIconPushButton.py
+++ pyqt_svg_icon_pushbutton/svgIconPushButton.py
@@ -1,6 +1,7 @@
-from PyQt5.QtGui import QColor, QPalette
+from PyQt5.QtGui import QColor, QPalette, qGray
 from PyQt5.QtWidgets import QPushButton, QGraphicsColorizeEffect, QWidget
 from python_get_absolute_resource_path.getAbsoulteResourcePath import get_absolute_resource_path
+from python_color_getter.pythonColorGetter import PythonColorGetter
 
 
 class SvgIconPushButton(QPushButton):
@@ -26,21 +27,36 @@ class SvgIconPushButton(QPushButton):
 
     def __initColorByBaseWidget(self):
         base_color = self.__baseWidget.palette().color(QPalette.Base)
-        hover_factor = 120
-        pressed_factor = 140
-        checked_factor = 130
-        if base_color.name() == '#ffffff':
-            self.__hover_color = base_color.darker(hover_factor).name()
-            self.__pressed_color = base_color.darker(pressed_factor).name()
-            self.__checked_color = base_color.darker(checked_factor).name()
+        self.__hover_color = self.__getHoverColor(base_color)
+        self.__pressed_color = self.__getPressedColor(base_color)
+        self.__checked_color = self.__getPressedColor(base_color)
+
+    def __getColorByFactor(self, base_color, factor):
+        r, g, b = base_color.red(), base_color.green(), base_color.blue()
+        gray = qGray(r, g, b)
+        if gray > 255 // 2:
+            color = base_color.darker(factor)
         else:
-            self.__hover_color = base_color.lighter(hover_factor).name()
-            self.__pressed_color = base_color.lighter(pressed_factor).name()
-            self.__checked_color = base_color.lighter(checked_factor).name()
+            color = QColor(r+34, g+34, b+34)
+            color = color.lighter(factor)
+        return color
+
+    def __getHoverColor(self, base_color):
+        hover_factor = 130
+        hover_color = self.__getColorByFactor(base_color, hover_factor)
+        return hover_color.name()
+
+    def __getPressedColor(self, base_color):
+        pressed_factor = 140
+        pressed_color = self.__getColorByFactor(base_color, pressed_factor)
+        return pressed_color.name()
+
+    def __getCheckedColor(self, base_color):
+        return self.__getPressedColor(base_color)
 
     def __styleInit(self):
         self.__btn_style = f'''
-        QPushButton 
+        QPushButton
         {{
         border: 0;
         width: {self.__size};
