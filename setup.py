d5febd7f352bc82668be3f29d70c673af3b3adfd
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-09 18:37:04 +0900

    Update setup.py for pypi description

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-svg-icon-pushbutton',
-    version='0.7.1',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt QPushButton which user can set svg icon (not a fake low quality svg icon)',
     url='https://github.com/yjg30737/pyqt-svg-icon-pushbutton.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'python-get-absolute-resource-path @ git+https://git@github.com/yjg30737/python-get-absolute-resource-path.git@main'
