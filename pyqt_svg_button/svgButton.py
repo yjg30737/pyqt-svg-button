69114d1df4ac9139fd2a5d6e78b5ce1260e96722
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-22 14:55:43 +0900

    Fix the code, bump version (0.0.23 -> 0.0.24)

--- pyqt_svg_button/svgButton.py
+++ pyqt_svg_button/svgButton.py
@@ -4,4 +4,4 @@ from pyqt_svg_abstractbutton import SvgAbstractButton
 
 class SvgButton(QPushButton, SvgAbstractButton):
     def __init__(self, base_widget: QWidget = None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(base_widget, *args, **kwargs)
